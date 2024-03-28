79698e427c4821c8b69774e41a8b84a8fc189751
GuadaFachinotti <guadafachinotti@gmail.com>
2024-03-28 15:34:08 -0300

    Se agrega clase sortedlist

--- SortedListTP1/Class1.cs
+++ SortedListTP1/Class1.cs
@@ -1,7 +1,14 @@
 ﻿namespace SortedListTP1
 {
-    public class Class1
+    public class Concesionaria
     {
+        public SortedList<int, string> Vehiculos {  get; set; }
 
+
+        //Inicializamos vehículo
+        public Concesionaria() 
+        {
+            Vehiculos = new SortedList<int, string>();
+        }
     }
 }
